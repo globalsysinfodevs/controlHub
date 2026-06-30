22ddfed2e62b34d288d644aa56afc3bead393ef9
Kumara <co_kswamy@axtel.com.mx>
2026-06-30 22:08:17 +0530

    test deployement env variables
    test deployement env variables

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/query";
 import { router } from "@/routes";
 import { ToastViewport } from "@/components/ui/Toast";
+console.log(import.meta.env.VITE_API_BASE_URL,"test env");
+console.log(import.meta.env.VITE_USE_MOCK,"test env");
 
 export default function App() {
   return (
